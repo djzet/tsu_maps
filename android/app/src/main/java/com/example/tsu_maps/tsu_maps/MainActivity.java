91212fd1181bf78258fa81eb750d30cfc5fd9ba4
sea <silaevegor2003@mail.ru>
2026-03-19 21:02:18 +0700

    add icon, fonts

---
+++ android/app/src/main/java/com/example/tsu_maps/tsu_maps/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.tsu_maps.tsu_maps;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
--- android/app/src/main/kotlin/com/example/tsu_maps/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.tsu_maps
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
